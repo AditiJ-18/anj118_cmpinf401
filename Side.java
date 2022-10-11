815c2a397ddf04ea2050d61d53e86b9439932cdf
AditiJ-18 <87610070+AditiJ-18@users.noreply.github.com>
2022-10-11 13:55:01 -0400

    Add files via upload

---
+++ Side.java
@@ -0,0 +1,27 @@
+/** 
+ * Class Side 
+ * @author aditijunagade
+ * created: 10/11/2022 
+ */
+
+public class Side {
+
+	private String name; 
+	private String description; 
+	private int calories; 
+
+	public Side(String name, String desc, int cal) { 
+		this.name = name; 
+		this.description = desc; 
+		this.calories = cal; 
+	}
+
+	public int getSideCals() { 
+		return calories; 
+	}
+
+	public String getSideDesc() { 
+		return name + ". " + description; 
+	}
+
+}
